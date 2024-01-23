3c6cbc11012f05835cbedb50d4974875ae9962d1
varshamohandass <155077946+varshamohandass@users.noreply.github.com>
2024-01-23 07:13:01 +0530

    Add files via upload

--- movielist.py
+++ movielist.py
@@ -1,5 +1,5 @@
 import urllib.parse
-from datetime import datetime
+
 
 import requests
 import json
@@ -31,7 +31,7 @@ param['start_date'] = start_date
 param['end_date'] = end_date
 
 res = urllib.parse.urlencode(param, doseq=True)
-new_url = url +'&' + res
+new_url = url +'&' + res 
 
 headers = {
     "accept": "application/json",
@@ -42,11 +42,25 @@ response = requests.get(new_url, headers=headers)
 
 # print(response.text)
 cert = json.loads(response.text)
+url = "https://api.themoviedb.org/3/movie/changes"
+total_pages = cert["total_pages"]
+fieldname = ['page', 'id', 'adult']
 
-fieldname = ['id', 'adult']
-with open('movie_list.csv', 'w',encoding = "utf-8") as movie_list :
+for page in range(1,total_pages+1):
+   param["page"] = page
+   
+   res = urllib.parse.urlencode(param, doseq=True)
+   new_url = url + '?' + res
+   headers = {
+    "accept": "application/json",
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOTc4YjhlNzNmODVhMzJmNmI0MmNjZjI5YjU5OTg3NCIsInN1YiI6IjY1OWI0ZjBlODc0MWM0MDE0OWQwMjNhMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.LmPACCxVu3VwVZkPj1jogwL0nkH-8V55SIPMaJX1L1k"
+}
+   response = requests.get(new_url, headers=headers)
+   cert = json.loads(response.text)
+   with open(f'{start_date}_{end_date}.csv', 'a',encoding = "utf-8") as movie_list :
     csv_writer = csv.DictWriter(movie_list, fieldnames = fieldname)
     csv_writer.writeheader()
     for i in cert['results']:
+      i["page"] = page
       csv_writer.writerow(i)
     
